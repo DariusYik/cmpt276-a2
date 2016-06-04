e938261a562d554fdc0f6b1239b6e4b8ba46e88a
Darius <darius.yik@gmail.com>
2016-06-04 12:01:44 -0700

    testing out student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,10 @@
 class Student < ActiveRecord::Base
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :weight, presence: true, length: {minimum:1, maximum:3}
+  validates :height, presence: true, length: {minimum:1, maximum:3}
+  validates :color, presence: true
+  validates :gpa, presence: true, length: {minimum:1, maximum:3}
+  validates :student_number, presence: true, length: {minimum:9, maximum:9}
+
 end
